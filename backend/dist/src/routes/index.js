1744cfe806d64ca54bd88032c2a5bfdf9f152898
7t64vu3ge <timmyojha@gmail.com>
2026-03-30 17:34:29 +0530

    Cleanup: Remove node_modules and other ignored files from git

--- backend/dist/src/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import sketchfabRoutes from "./sketchfab.routes.js";
-import assetRoutes from "./asset.routes.js";
-const router = Router();
-// Mount all API routes here
-router.use("/sketchfab", sketchfabRoutes);
-router.use("/assets", assetRoutes);
-export default router;
